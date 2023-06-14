3e213e18e3d94e80869c05c24c7d973441aea160
Kevin Tonnis <kevin.tonnis@gmail.com>
2023-06-14 13:03:50 -0400

    demo

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ const PDFParser = require("pdf2json");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let pdfParser = new PDFParser(this, 1);
-    pdfParser.loadPDF(`PDF.pdf`);
-    pdfParser.on("pdfParser_dataReady", (pdfData) => {
-        res.status(200).send(pdfParser.getRawTextContent())
-    })
+  // let pdfParser = new PDFParser(this, 1);
+  //   pdfParser.loadPDF(`PDF.pdf`);
+  //   pdfParser.on("pdfParser_dataReady", (pdfData) => {
+  //       res.status(200).send(pdfParser.getRawTextContent())
+  //   })
+  res.status(200).send('hello')
 });
 
 module.exports = router;
