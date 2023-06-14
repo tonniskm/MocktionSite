9ca7d165d2bd3b4c67a7c76b0bb7c2cb403d623a
Kevin Tonnis <kevin.tonnis@gmail.com>
2023-06-14 13:10:48 -0400

    demo

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,10 @@ router.get('/', function(req, res, next) {
     pdfParser.loadPDF(`PDF.pdf`);
     pdfParser.on("pdfParser_dataReady", (pdfData) => {
         // res.status(200).send(pdfParser.getRawTextContent())
-        res.status(200).send('hi')
+        // res.status(200).send('hi')
     })
-  // res.status(200).send('hello2')
+    console.log('hi')
+  res.status(200).send('hello2')
 });
 
 module.exports = router;
