8b093b1d28109dbd304a9ef77bbf329efa72fcef
Kevin Tonnis <kevin.tonnis@gmail.com>
2023-06-14 15:42:13 -0400

    demo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const PDFParser = require("pdf2json");
+// const PDFParser = require("pdf2json");
+const PDFParser = require('pdf-parse');
+const pdfUrl = 'https://g.espncdn.com/s/ffldraftkit/22/NFLDK2022_CS_NonPPR300.pdf';
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,7 +13,19 @@ router.get('/', function(req, res, next) {
     //     // res.status(200).send(pdfParser.getRawTextContent())
     //     // res.status(200).send('hi')
     // })
-  res.status(200).send('hello3')
+  // res.status(200).send('hello3')
+  request({ url: pdfUrl, encoding: null }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const options = {};
+      PDFParser(body, options)
+        .then(function(data) {
+            res.status(200).send(data)
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+    }
+  });
 });
 
 module.exports = router;
